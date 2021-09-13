620ece49bc8392d5ca2b7addfe37e27a90b30406
freekgorrissen <f.gorrissen@gmail.com>
2021-09-13 16:33:20 +0200

    Start van pokemon analyse Freek

--- freekscript.py
+++ freekscript.py
@@ -1,1 +1,38 @@
-print("eerste regel")
+
+import pandas as pd ## tabel package
+import csv
+
+# import requests
+# from bs4 import BeautifulSoup
+#
+# URL = 'https://coinmarketcap.com/'
+# page = requests.get(URL)
+#
+# soup = BeautifulSoup(page.content, 'html.parser')
+# tabel = soup.find('tbody')
+# rijen = tabel.find_all('tr')
+#
+# class Belegging:
+#     naam = 'onbekend'
+#     prijs = -1
+#
+# beleggingen = []
+#
+# for elems in range(5):
+#     belegging = Belegging()
+#     titel = rijen[elems].find_all('p')
+#     belegging.naam = titel[1].text
+#     prijs = rijen[elems].find_all('a')
+#     belegging.prijs = prijs[1].text
+#     beleggingen.append(belegging)
+#
+# #print(beleggingen)
+#
+# for b in beleggingen:
+#     print(b.naam)
+#     print(b.prijs)
+
+
+pokcsv = pd.read_csv("C:/Users/fgorr/OneDrive/Desktop/Traineeship/Python/Pokemon.csv")
+
+print(pokcsv[pokcsv.Generation == 1])
