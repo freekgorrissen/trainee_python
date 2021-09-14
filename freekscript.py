9d2bb9d6fcda5bb1253c7923060c42730984495e
freekgorrissen <f.gorrissen@gmail.com>
2021-09-14 11:40:56 +0200

    pokemon loaded Freek

--- freekscript.py
+++ freekscript.py
@@ -1,3 +1,21 @@
-print("eerste regel")
+import pandas as pd
+import matplotlib as mpl
 
-# test
+pok = pd.read_csv("Pokemon.csv")
+
+trimnames = []
+
+# 
+for col in pok.columns:
+    nm = ""
+    for l in col:
+        if not l in ". ":
+            nm += l
+    trimnames.append(nm)
+
+trimnames[0] = "Number"
+pok.columns = trimnames
+
+numval = ["HP", "Speed", "Attack", "Defense"]
+
+pok_gen = pd.DataFrame(pok.Generation.unique())
